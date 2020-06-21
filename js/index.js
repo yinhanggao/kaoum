aa03b34531470a27de79b8a7875e5dc2facb083a
yinhanggao <62370528+yinhanggao@users.noreply.github.com>
2020-06-21 12:57:51 +0800

    0621

--- js/index.js
+++ js/index.js
@@ -1,33 +1,24 @@
 $("path").mouseup(function (e) {
   var tagname = $(this).attr("data-name");
-  $.ajax({
-    type: "GET",
-    url:
-      "https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-D0047-089?Authorization=CWB-5FC93907-783A-4AC3-8BC1-70D286425F47&locationName=" +
-      tagname,
-    success: function (data) {
-      var thisdata = data;
 
-      /* console.log(
-          thisdata["records"]["locations"][0]["location"][0]["locationName"]
-        );*/
-      str =
-        thisdata["records"]["locations"][0]["location"][0]["weatherElement"][7][
-          "time"
-        ][0]["elementValue"][0]["value"];
-      int =
-        thisdata["records"]["locations"][0]["location"][0]["weatherElement"][1][
-          "time"
-        ][0]["elementValue"][0]["value"];
-      dex =
-        thisdata["records"]["locations"][0]["location"][0]["weatherElement"][3][
-          "time"
-        ][0]["elementValue"][0]["value"];
-      //$('#content').append(str);
-      document.getElementById("citty").innerHTML = tagname;
-      document.getElementById("weath12h0").innerHTML = "☂" + str + "%";
-      document.getElementById("weath12h1").innerHTML = int;
-      document.getElementById("weath12h3").innerHTML = dex + "&#176;";
-    },
-  });
+  var xhr = new XMLHttpRequest();
+  xhr.open(
+    "GET",
+    "https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-D0047-089?Authorization=CWB-5FC93907-783A-4AC3-8BC1-70D286425F47&locationName=" +
+      tagname
+  );
+  xhr.send(null);
+  xhr.onload = function () {
+    var thisdata = JSON.parse(xhr.responseText);
+    thisdata =
+      thisdata["records"]["locations"][0]["location"][0]["weatherElement"];
+    str = thisdata[7]["time"][0]["elementValue"][0]["value"];
+    int = thisdata[1]["time"][0]["elementValue"][0]["value"];
+    dex = thisdata[3]["time"][0]["elementValue"][0]["value"];
+    //$('#content').append(str);
+    document.getElementById("citty").innerHTML = tagname;
+    document.getElementById("weath12h0").innerHTML = "☂" + str + "%";
+    document.getElementById("weath12h1").innerHTML = int;
+    document.getElementById("weath12h3").innerHTML = dex + "&#176;";
+  };
 });
